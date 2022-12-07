1219fae09f3f954785b43c72a36a71c61f279ca6
muideen7 <olayeyeayomide2000@gmail.com>
2022-12-07 21:59:37 +0100

    Day 1

---
+++ string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Write your code below this line 
+print("Hello World!\nHello World!\nHello World!")
+print("Hello" " " + "Ayomide")
+print("Hello " + "Olamide")
+print("Hello" + " " + "Muideen")
