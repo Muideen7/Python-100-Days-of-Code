1e00991dd48a2f7a7d1e67c47e4ff98e34e294f6
muideen7 <olayeyeayomide2000@gmail.com>
2022-12-08 07:05:38 +0100

    First python program

---
+++ swap.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# create a third variable amd put the content of variable a in it in order to make it easier to swap the value of a and b
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+default = a
+a = b
+b = default
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
+
