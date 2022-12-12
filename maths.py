28b647972c660ea0d7a3373c85b709da3d371fb7
muideen7 <olayeyeayomide2000@gmail.com>
2022-12-12 06:05:22 +0100

    Day 2

---
+++ maths.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+3 + 5
+7 - 4
+3 * 2
+6 / 3
+2 ** 3
+
+PEMDAS
+P = Parentheses () 
+E = Exponents **
+M = Multiplication * 
+D = Division / 
+A = Addition + 
+S = Subtraction - 
+
+print(3 *3 + 3 / 3 - 3)
